64af584a5a5869a105a3939810519c8be5d307c8
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-08-02 19:43:22 -0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>To-do App</h1>
+        <ul>
+          <li>
+            <input type="checkbox" name="" id="" />
+            <h2>thing to do</h2>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
