e8dc8a86b65e4186d5256e88de61846422b16251
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-08-02 20:21:06 -0300

    create ListComponent Component and useState to update the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { ListComponent } from "./ListComponent";
+
 import "./index.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
   return (
     <>
       <div className="container">
         <h1>To-do App</h1>
+        <form>
+          <label htmlFor="insert-to-do"></label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="insert-to-do"
+            name="To-do"
+            placeholder="Insert a to-do item"
+          />
+          <button>Add</button>
+        </form>
         <ul>
-          <li>
-            <input type="checkbox" name="" id="" />
-            <h2>thing to do</h2>
-          </li>
+          <ListComponent />
         </ul>
       </div>
     </>
---
+++ src/ListComponent.jsx
@@ -0,0 +1,9 @@
+export const ListComponent = () => {
+  return (
+    <li>
+      <input type="checkbox" name="" id="" />
+      <h2>thing to do</h2>
+      <button className="delete">Delete</button>
+    </li>
+  );
+};
